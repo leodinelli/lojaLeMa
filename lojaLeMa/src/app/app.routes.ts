f38079c3398edc01aeeac3c3ce1f53b1442a971d
Leonardo Dinelli <leodinelli2007@gmail.com>
2025-04-08 06:24:54 -0300

    Posting a new branch
    commit for just post my new branch

--- lojaLeMa/src/app/app.routes.ts
+++ lojaLeMa/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { VitrineComponent } from './vitrine/vitrine.component';
+import { DetalheComponent } from './detalhe/detalhe.component';
+export const routes: Routes = [
+    {path:"detalhe", component:DetalheComponent},
+    {path:"vitrine", component:VitrineComponent},
+    {path:"", component:VitrineComponent}
+];
--- lojaLeMa/src/app/vitrine/vitrine.component.ts
+++ lojaLeMa/src/app/vitrine/vitrine.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { Produto } from '../model/produto';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-vitrine',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './vitrine.component.html',
   styleUrl: './vitrine.component.css'
 })
 export class VitrineComponent {
-
-}
+  public mensagem: string = "";
+  public lista: Produto[] = [
+    {codigo:1, nome:"Bateria", valor: 12800, descritivo: "Bateria preta", quantidade:1, cupom:"Leleo", keywords:"", marca:"Ludwig"},
+    {codigo:2, nome:"Violão", valor: 5000, descritivo: "Violão marrom", quantidade:3, cupom:"Mathias", keywords:"", marca:"Takamine"},
+    {codigo:3, nome:"Cuica", valor: 500, descritivo: "Muito daora", quantidade:10, cupom:"Leleo", keywords:"", marca:"BATUKA"}
+  ];
+} 
