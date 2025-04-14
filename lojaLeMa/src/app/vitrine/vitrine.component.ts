1e89f04ac4703146c459219991159ea571309228
Leonardo Dinelli <leodinelli2007@gmail.com>
2025-04-14 07:10:52 -0300

    i dont no what you did

--- lojaLeMa/src/app/vitrine/vitrine.component.ts
+++ lojaLeMa/src/app/vitrine/vitrine.component.ts
@@ -12,7 +12,7 @@ export class VitrineComponent {
   public mensagem: string = "";
   public lista: Produto[] = [
     {codigo:1, nome:"Bateria", valor: 12800, descritivo: "Bateria preta", quantidade:1, cupom:"Leleo", keywords:"", marca:"Ludwig"},
-    {codigo:2, nome:"Violão", valor: 5000, descritivo: "Violão marrom", quantidade:3, cupom:"Mathias", keywords:"", marca:"Takamine"},
+    {codigo:2, nome:"Violão", valor: 5000, descritivo: "Violão marrom", quantidade:3, cupom:"Mathias", keywords:"", marca:"Taylor"},
     {codigo:3, nome:"Cuica", valor: 500, descritivo: "Muito daora", quantidade:10, cupom:"Leleo", keywords:"", marca:"BATUKA"}
   ];
 } 
